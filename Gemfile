1fade0e2614be1b7955e126643c6afad2b959914
Henrik Sjökvist <henrik.sjokvist@gmail.com>
2013-10-09 14:28:35 +0200

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source "https://rubygems.org"
+source 'https://sr9bdZCARFKkWwxW2in4@gem.fury.io/kollegorna/'
+
+gem "lita"
+
+# Uncomment to use the HipChat adapter
+gem "lita-hipchat"
+
+# Uncomment to use the IRC adapter
+# gem "lita-irc"
+
+# Add handlers to give Lita new functionality.
+# For example:
+gem "lita-karma"
+gem "lita-animate"
+gem "lita-memegen"
+gem "lita-google-images"
+gem "lita-flip"
+gem "lita-eightball"
+gem "lita-excuses"
+gem "lita-coin"
+gem 'lita-helen'
+# gem 'lita-helen', path: '/Users/henrik/Projects/lita-helen'
---
+++ lita_config.rb
@@ -0,0 +1,36 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "Kobot"
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :debug
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :hipchat
+  config.adapter.jid = ENV['HIPCHAT_JID']
+  config.adapter.password = ENV['HIPCHAT_PASSWORD']
+  config.adapter.debug = false
+  config.adapter.rooms = :all
+
+  ## Example: Set options for the chosen adapter.
+  # config.adapter.username = "myname"
+  # config.adapter.password = "secret"
+
+  ## Example: Set options for the Redis connection.
+  # config.redis.host = "127.0.0.1"
+  # config.redis.port = 1234
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+  config.handlers.helen.api_key = ENV['HELEN_API_KEY']
+  config.handlers.helen.host = ENV['HELEN_HOST']
+end
